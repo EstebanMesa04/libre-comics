496cbfe1d6707bf4edfe81ad83f262a20b1ddd3f
Esteban Mesa <esteban.mesaa@uniagustiniana.edu.co>
2026-05-25 19:59:40 -0500

    se añadio un boton para ocultar o mostrar panel cuando se oculta la barra de estado

--- src/pages/Lector/Lector.jsx
+++ src/pages/Lector/Lector.jsx
@@ -178,6 +178,24 @@ function Lector() {
         cargando={cargando}
         error={error}
       />
+
+      {/* boton para ocultar o mostrar panel cuando se oculta la barra de estado */}
+
+      <svg
+        onClick={() => {
+          setMostrarPanel(!mostrarPanel);
+        }}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className={`bar-icon ${mostrarBarra ? "bar-icon-ocultar" : ""}`}
+      >
+        <path
+          fillRule="evenodd"
+          d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75H12a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
+          clipRule="evenodd"
+        />
+      </svg>
     </main>
   );
 }
